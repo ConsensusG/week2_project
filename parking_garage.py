7a905e3842783fe2c56bcea976e252339bf6898e
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 <Jmandeville@protonmail.com>
2023-07-30 16:33:21 -0400

    first commit

---
+++ parking_garage.py
@@ -0,0 +1,19 @@
+class Parking_Garage:
+    def __init__(self):
+        pass
+
+    def takeTicket(self):
+        pass
+
+    def payForParking(self):
+        pass
+
+    def leaveGarage(self):
+        pass
+
+   
+    def view_current_tickets(self):
+        pass
+
+
+test = Parking_Garage()
